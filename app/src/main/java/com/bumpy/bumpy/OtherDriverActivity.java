e635f31e1a446d78fdcc562b018d7bc226dad6b4
ron st <ronst12@gmail.com>
2018-01-10 18:45:21 +0200

    Yay

--- app/src/main/java/com/bumpy/bumpy/OtherDriverActivity.java
+++ app/src/main/java/com/bumpy/bumpy/OtherDriverActivity.java
@@ -1,8 +1,21 @@
 package com.bumpy.bumpy;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -11,6 +24,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -21,8 +35,21 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class OtherDriverActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
+public class OtherDriverActivity extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback {
+
+    public static final String ERROR_DETECTED = "No NFC tag detected!";
+    public static final String WRITE_SUCCESS = "Text written to the NFC tag successfully!";
+    public static final String WRITE_ERROR = "Error during writing, is the NFC tag close enough to your device?";
+    public static final int MY_SOCKET_TIMEOUT_MS = 5000;
+    NfcAdapter nfcAdapter;
+    PendingIntent pendingIntent;
+    IntentFilter writeTagFilters[];
+    boolean writeMode;
+    Tag myTag;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +57,48 @@ public class OtherDriverActivity extends AppCompatActivity {
         setContentView(R.layout.activity_other_driver);
         Toolbar myToolbar = findViewById(R.id.custom_toolbar);
         setSupportActionBar(myToolbar);
+        context = this;
+        Toast.makeText(this, "Waiting for nfc", Toast.LENGTH_LONG).show();
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+        }
+        if (!nfcAdapter.isEnabled()) {
+            Toast.makeText(this, "Please enable NFC via Settings.", Toast.LENGTH_LONG).show();
+        }
+        nfcAdapter.setNdefPushMessageCallback(this, this);
+
+//        readFromIntent(getIntent());
+//
+//        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+//        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+//        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+//        writeTagFilters = new IntentFilter[] { tagDetected };
+//        try {
+//            write("Hey amnonn it work1!!", myTag);
+//        } catch (Exception e)
+//        {
+//            Toast.makeText(this, "Failed to nfc", Toast.LENGTH_LONG).show();
+//        }
 
     }
 
     @Override
+    public NdefMessage createNdefMessage(NfcEvent nfcEvent) {
+        Toast.makeText(this, "Sending a message to you", Toast.LENGTH_LONG).show();
+        String message = "Blabla amnonn!!!";
+        NdefRecord ndefRecord = NdefRecord.createMime("text/plain", message.getBytes());
+        NdefMessage ndefMessage = new NdefMessage(ndefRecord);
+        return ndefMessage;
+    }
+
+    public void NFC(View view) {
+
+    }
+
+        @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
@@ -113,7 +178,7 @@ public class OtherDriverActivity extends AppCompatActivity {
         }
 
         RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-        String url = "http://192.168.1.36:65432/v1/accident";
+        String url = "http://192.168.43.252:65432/v1/accident";
         JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
                 url, postparams,
                 new Response.Listener() {
@@ -136,7 +201,27 @@ public class OtherDriverActivity extends AppCompatActivity {
 
                     }
                 });
+        jsonObjReq.setRetryPolicy(new DefaultRetryPolicy(
+                MY_SOCKET_TIMEOUT_MS,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
 
         queue.add(jsonObjReq);
     }
+
+protected void onResume(){
+    super.onResume();
+    Intent intent = getIntent();
+    if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+        Parcelable[] rawMessages = intent.getParcelableArrayExtra(
+                NfcAdapter.EXTRA_NDEF_MESSAGES);
+
+        NdefMessage message = (NdefMessage) rawMessages[0]; // only one message transferred
+        Toast.makeText(this, new String(message.getRecords()[0].getPayload()), Toast.LENGTH_LONG).show();
+
+    } else
+        Toast.makeText(this, "Waiting for NDEF Message", Toast.LENGTH_LONG).show();
+
+}
+
 }
