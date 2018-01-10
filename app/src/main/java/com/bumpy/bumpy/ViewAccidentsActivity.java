92f5ca33f0d059f856f02157fc848651c34998c8
amnon <amnon1995@gmail.com>
2018-01-11 00:14:36 +0200

    simple listview

--- app/src/main/java/com/bumpy/bumpy/ViewAccidentsActivity.java
+++ app/src/main/java/com/bumpy/bumpy/ViewAccidentsActivity.java
@@ -1,14 +1,25 @@
 package com.bumpy.bumpy;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.PrintWriter;
 
 public class ViewAccidentsActivity extends AppCompatActivity {
 
-    String[] stringArray = {"c", "b", "c"};
+    String[] datesArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +27,21 @@ public class ViewAccidentsActivity extends AppCompatActivity {
         Toolbar myToolbar = findViewById(R.id.custom_toolbar);
         setSupportActionBar(myToolbar);
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.accident_item, stringArray);
+
+        JSONObject json;
+        try {
+            json = new JSONObject("{result : [a, b, ladlf, lfo]}");
+            JSONArray jsonDates = json.getJSONArray("result");
+            datesArray = new String[jsonDates.length()];
+            for (int i = 0; i < datesArray.length; i++) {
+                datesArray[i] = jsonDates.optString(i);
+            }
+
+        } catch (JSONException e) {
+            Log.d("ERROR", e.getMessage());
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.accident_item, datesArray);
 
         ListView listView = (ListView) findViewById(R.id.accidents);
         listView.setAdapter(adapter);
