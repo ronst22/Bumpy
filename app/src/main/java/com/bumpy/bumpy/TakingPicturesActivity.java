fd4a3ff4ff89d492b44b5b490826d0bb36f1e853
amitk <eylon0060@gmail.com>
2018-03-23 17:21:34 +0300

    rebase rotate pic

--- app/src/main/java/com/bumpy/bumpy/TakingPicturesActivity.java
+++ app/src/main/java/com/bumpy/bumpy/TakingPicturesActivity.java
@@ -2,6 +2,7 @@ package com.bumpy.bumpy;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -40,7 +41,16 @@ public class TakingPicturesActivity extends BaseBumpyActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            BitmapDrawable drawable = new BitmapDrawable(getResources(), imageBitmap);
+
+            // Rotate the image (I dont know why its rotated in the first place)
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
+            Bitmap scaledBitmap = Bitmap.createScaledBitmap(imageBitmap, imageBitmap.getWidth(),
+                                                            imageBitmap.getHeight(),true);
+            Bitmap rotatedBitmap = Bitmap.createBitmap(scaledBitmap , 0, 0, scaledBitmap .getWidth(), scaledBitmap .getHeight(), matrix, true);
+
+            // Put the image in the button
+            BitmapDrawable drawable = new BitmapDrawable(getResources(), rotatedBitmap);
             img.setBackground(drawable);
         }
     }
